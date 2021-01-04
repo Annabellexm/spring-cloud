d50315ad83530d3317601a83be9213e2ddc62b7f
Meng.Xu <1066114837@qq.com>
2021-01-04 16:42:28 +0800

    spring cloud +Eureka+Consul+Ribbon+OpenFegin

--- cloud-provider-payment8001/src/main/java/com/cn/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/cn/controller/PaymentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
@@ -67,6 +68,17 @@ public class PaymentController {
  @GetMapping("/payment/lb")
  public String getPaymentLB(){
   return serverPort;
+ }
+
+ @GetMapping(value = "/payment/feign/timeout")
+ public String PaymentTimeOut(){
+  //暂停几秒钟线程
+  try {
+   TimeUnit.SECONDS.sleep(3);
+  } catch (Exception e) {
+   e.printStackTrace();
+  }
+   return serverPort;
  }
  
 
