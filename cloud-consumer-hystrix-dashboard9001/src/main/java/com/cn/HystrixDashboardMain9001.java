50a2044ee210c5d4f81d9cacfe892a0a1380561f
Meng.Xu <1066114837@qq.com>
2021-01-06 15:35:25 +0800

    spring cloud +Eureka+Consul+Ribbon+OpenFegin+Hystrix

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/cn/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.cn;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+ public static void main(String[] args) {
+  SpringApplication.run(HystrixDashboardMain9001.class,args);
+ }
+
+
+}
